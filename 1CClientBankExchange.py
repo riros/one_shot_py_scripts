e661e90a431d4c1ebf3e5046f3ee727ba75aa30e
Ivan Valenkov <ivanvalenkov@gmail.com>
2016-11-28 12:28:39 +0300

    Update 1CClientBankExchange.py

--- 1CClientBankExchange.py
+++ 1CClientBankExchange.py
@@ -3,13 +3,14 @@ import xlsxwriter
 import datetime
 import re
 
-"""
-TODO 1) Сделать авто конвертацию дат и времени на сеттерах. если в имени атрибута есть date
-"""
+
 class CBaseLib:
+    _loaded = False
+    _fields = []
+
     def __init__(self):
         self._fields = {}
-        self.loaded = False
+        self._loaded = False
 
     @property
     def fields(self):
@@ -25,15 +26,45 @@ class CBaseLib:
     def load_fields_from_string(self, s):
         for k, v in self.fields.items():
             try:
-                self.__dict__[k] = re.search(v, s).groups()[0]
+                self.__setattr__(k, re.search(v, s).groups()[0])
             except AttributeError:
                 print('не найдено одно из полей %ы. %s = %s', self.__class__, k, v)
                 self.__dict__[k] = None
+        self.set_loaded()
         return self
+
+    def __setattr__(self, key, value):
+        if isinstance(value, ''.__class__) and len(value) <= 12:
+            try:
+                v = float(value)
+            except:
+                pass
+            else:
+                self.__dict__[key] = v
+                return self
+            if re.match('\d{2}.\d{2}.\d{4}', value):
+                try:
+                    self.__dict__[key] = datetime.datetime.strptime(value, '%d.%m.%Y').strftime('%Y-%m-%d')
+                    return self
+                except:
+                    print("ошибка конвертации даты %s", value)
+                    raise Exception()
+
+        self.__dict__[key] = value
+
+    @classmethod
+    def is_loaded(cls):
+        return cls._loaded
+
+    @classmethod
+    def set_loaded(cls, state=True):
+        cls._loaded = state
 
 
 class CBankStatementDoc(CBaseLib):
     """docstring for EDoc"""
+    x_PayerINN = ''
+    x_PayerAccount = ''
 
     def __init__(self):
         super(CBankStatementDoc, self).__init__()
@@ -83,13 +114,14 @@ class CBankStatementDoc(CBaseLib):
     def iterator_from_string(self, src_str):
         s = src_str[:]
         while True:
-            docStart = re.search("СекцияДокумент", s)
-            docEnd = re.search("КонецДокумента", s)
-            if docEnd and docStart:
-                str_DocSection = s[docStart.span()[0]:docEnd.span()[0]]
+            doc_start = re.search("СекцияДокумент", s)
+            doc_end = re.search("КонецДокумента", s)
+            if doc_end and doc_start:
+                str_DocSection = s[doc_start.span()[0]:doc_end.span()[0]]
                 self.load_fields_from_string(str_DocSection)
-                s = s[:docStart.span()[0]] + s[docEnd.span()[1]:]
+                s = s[:doc_start.span()[0]] + s[doc_end.span()[1]:]
             else:
+                self.set_loaded(False)
                 break
             yield self
 
@@ -102,7 +134,7 @@ class CAccount(CBaseLib):
         self.x_dateBegin = "\nДатаНачала=(.*)"
         self.x_dateEnd = "\nДатаКонца=(.*)"
         self.x_account = "\nРасчСчет=(.*)"
-        self.x_balanceBegin = "\nНачальныйОстаток=(.*)"
+        self.x_balanceBegin = "НачальныйОстаток=(.*)"
         self.x_amount_summ = "\nВсегоПоступило=(.*)"
         self.x_discarded_summ = "\nВсегоСписано=(.*)"
         self.x_balanceEnd = "\nКонечныйОстаток=(.*)"
@@ -121,7 +153,8 @@ class CAccount(CBaseLib):
 class CBankStatement(CBaseLib):
     """docstring for BankStatement"""
     """TODO: настройка кодировки файла"""
-    def __init__(self):
+
+    def __init__(self, owner_account):
         super(CBankStatement, self).__init__()
         self.x_codepage = 'Кодировка=(.*)'
         self.x_version = 'ВерсияФормата=(.*)'
@@ -131,8 +164,10 @@ class CBankStatement(CBaseLib):
         self.x_timeCreate = '\nВремяСоздания=(.*)'
         self.x_account = 'РасчСчет=(.+)'
 
+        self.owner_account = owner_account
+
         self.Account = CAccount()
-        self.bank_statement_docs = iter([])
+        self.bank_statement_docs = iter([CBankStatementDoc])
         self.doc_ptr = CBankStatementDoc()
 
     def load_class_form_1CClientBankExchange_string(self, s):
@@ -142,50 +177,59 @@ class CBankStatement(CBaseLib):
             ss = self.Account.extract_one(m.groups()[0])
             self.bank_statement_docs = self.doc_ptr.iterator_from_string(ss)
             self.load_fields_from_string(ss)
-            strdatetime = self.x_dateCraate + ' ' + self.x_timeCreate
-            """25.10.2016 16:22:44"""
-            self.dateCraate = datetime.datetime.strptime(strdatetime, "%d.%m.%Y %H:%M:%S")
+            if self.x_dateCraate and self.x_timeCreate:
+                strdatetime = self.x_dateCraate + ' ' + self.x_timeCreate
+                self.dateCraate = datetime.datetime.strptime(strdatetime, "%Y-%m-%d %H:%M:%S")
+                self.set_loaded()
         else:
             raise Exception("не найден начальный и конечный тег файла. 1CClientBankExchange ... КонецФайла")
 
-
     def validate(self):
-        return True
-        if self.codepage == "Windows" and self.version == "1.02":
+        if self.x_codepage == "Windows" and self.x_version == "1.02":
             return True
         else:
             raise Exception("несоответствие формата файла загрузки")
 
-    def save_Documents_to_xlsx(self, filepath):
-        if not filepath:
+    def save_documents_to_xlsx(self, file_path):
+        if not file_path:
             raise Exception('Не указано имя файла для экспорта')
 
-        book = xlsxwriter.Workbook(filepath)
-        sheet = book.add_worksheet()
+        book = xlsxwriter.Workbook(file_path)
+        in_sheet = book.add_worksheet("приход")
+        out_sheet = book.add_worksheet('расход')
         first = True
-        c, r = 0, 0
+        c, in_r, out_r = 0, 0, 0
         for doc in self.bank_statement_docs:
             if first:
                 for k, v in doc.fields.items():
-                    sheet.write(r, c, v)
+                    in_sheet.write(in_r, c, v)
+                    out_sheet.write(out_r, c, v)
                     c += 1
                 c = 0
                 first = False
+            if doc.x_PayerAccount == self.owner_account:
+                out_r += 1
             else:
-                r += 1
-                for k, v in doc.fields.items():
-                    sheet.write(r, c, doc.__dict__[k])
-                    c += 1
-                c = 0
+                in_r += 1
+            for k, v in doc.fields.items():
+                if doc.x_PayerAccount == self.owner_account:
+                    out_sheet.write(out_r, c, doc.__dict__[k])
+                else:
+                    in_sheet.write(in_r, c, doc.__dict__[k])
+                c += 1
+            c = 0
         book.close()
+
+    def is_loaded(self):
+        return True if self.Account.is_loaded() and self._loaded else False
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    filename = "kikuban"
+    filename = "kikuban.txt"
     f = open(filename)
     filedata = f.read()
     f.close()
-    bs = CBankStatement()
+    bs = CBankStatement('40702810500200000540')
     bs.load_class_form_1CClientBankExchange_string(filedata)
-    bs.save_Documents_to_xlsx(filename + ".xlsx")
+    bs.save_documents_to_xlsx(filename + ".xlsx")
